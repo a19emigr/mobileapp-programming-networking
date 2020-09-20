d41ff10c9ffbc568666b9963fb7ce7ed86aaeca7
Emilia Gran <a19emigr@student.his.se>
2020-09-20 23:59:40 +0200

    Added a Mountain class

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,7 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+    private String[] mountainName={"Matterhorn","Mont Blanc","Denali"};
+    private String[] locationName={"Alps","Alps","Alaska"};
+    private int[] mountainHeight={4478,4808,6190};
+
+    private ArrayList<String> listData=new ArrayList<>(Arrays.asList(mountainName));
+    private ArrayList<Mountain> mountainArrayList=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
